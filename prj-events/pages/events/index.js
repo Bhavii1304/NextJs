e68beda0915885323df824499cc5bfeb56b83727
BhavikaThawani <bhavika.t@simformsolutions.com>
2024-07-22 17:24:48 +0530

    added filter functionality with its components

--- prj-events/pages/events/index.js
+++ prj-events/pages/events/index.js
@@ -1,11 +1,20 @@
 import { getAllEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
     <div>
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </div>
   );
